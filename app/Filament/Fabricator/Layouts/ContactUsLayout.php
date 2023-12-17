c01f5191d95cbc723984cbdbfafac8fd5e98f00b
sub-acc-ni-carlo <johncarloevasco55@gmail.com>
2023-12-17 17:18:58 +0800

    updated contact us

---
+++ app/Filament/Fabricator/Layouts/ContactUsLayout.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Fabricator\Layouts;
+
+use Z3d0X\FilamentFabricator\Layouts\Layout;
+
+class ContactUsLayout extends Layout
+{
+    protected static ?string $name = 'contact-us';
+}
