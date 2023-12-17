b7d869a805b8266b6e18d5cf67a912c94b0b3540
sub-acc-ni-carlo <johncarloevasco55@gmail.com>
2023-12-17 16:01:58 +0800

    updated

--- resources/views/components/filament-fabricator/page-blocks/product-page.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/product-page.blade.php
@@ -36,7 +36,7 @@
   <div class="home-product-row" style="background-color: white;">
     @foreach ($product_list as $product)
       <div class="home-product-col">
-        <img src="{{ asset('storage/'.$product->image_file) }} />
+        <img src="{{ asset('storage/'.$product->image_file) }}" />
         <h2 class="testi-name">{{ $product->model_name }}</h2>
         <div class="c3-buttons">
           <a href="/products/product-specs/{{$product->id}}">View Full Specs</a>
