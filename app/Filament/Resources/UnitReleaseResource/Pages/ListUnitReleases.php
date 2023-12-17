f3ce40b841e93315477944dc27aab610ac1a5301
sub-acc-ni-carlo <johncarloevasco55@gmail.com>
2023-12-18 07:53:34 +0800

    update, removed  created action

--- app/Filament/Resources/UnitReleaseResource/Pages/ListUnitReleases.php
+++ app/Filament/Resources/UnitReleaseResource/Pages/ListUnitReleases.php
@@ -15,7 +15,7 @@ class ListUnitReleases extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 
