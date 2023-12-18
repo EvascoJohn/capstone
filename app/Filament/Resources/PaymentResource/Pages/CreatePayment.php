d93c50652a7561a6b0ea6e08d0625feeff3631b0
sub-acc-ni-carlo <johncarloevasco55@gmail.com>
2023-12-18 08:29:07 +0800

    application

--- app/Filament/Resources/PaymentResource/Pages/CreatePayment.php
+++ app/Filament/Resources/PaymentResource/Pages/CreatePayment.php
@@ -26,17 +26,17 @@ class CreatePayment extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        // $application = CustomerApplication::find($data['customer_application_id'])->get();
-        // if($application->hasMonthlyPayment() == false)//Initial Payment (Down payment)
-        // {
-        //     $application->unit_amort_fin = Payment::calculateAmountMonthlyPayment(
-        //             $application->unit_srp,
-        //             $application->unit_ttl_dp,
-        //             $application->unit_term,
-        //             0.0, // monthly interest rate
-        //     );
-        // }
-        // $application->save();
+        $application = CustomerApplication::find($data['customer_application_id'])->get();
+        if($application->hasMonthlyPayment() == false)//Initial Payment (Down payment)
+        {
+            $application->unit_amort_fin = Payment::calculateAmountMonthlyPayment(
+                    $application->unit_srp,
+                    $application->unit_ttl_dp,
+                    $application->unit_term,
+                    0.0, // monthly interest rate
+            );
+        }
+        $application->save();
         return $data;
     }
 
