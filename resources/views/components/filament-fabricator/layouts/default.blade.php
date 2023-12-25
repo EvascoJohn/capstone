f8c3eceb3e8fa4f30ffffc3743e3ae38cc2faeac
sub-acc-ni-carlo <johncarloevasco55@gmail.com>
2023-12-25 21:12:35 +0800

    fixed copyrights

--- resources/views/components/filament-fabricator/layouts/default.blade.php
+++ resources/views/components/filament-fabricator/layouts/default.blade.php
@@ -123,7 +123,7 @@
         </div>
       </div>
       <div class="bottom">
-        <p>Copyright © 2023 <span>BrandName</span> Made by Group5</p>
+        <p>Copyright © 2023 <span>{{ env('APP_NAME') }}</span> Made by Group5</p>
       </div>
     </footer>
      {{-- Footer Here --}}
