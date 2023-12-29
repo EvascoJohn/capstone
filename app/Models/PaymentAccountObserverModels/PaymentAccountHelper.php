a1aaf9bb185476a590d73569dbd8d67cafa9b6d7
JeffAlteza <jepoyalteza@gmail.com>
2023-12-29 16:40:00 +0800

    updating payment details

--- app/Models/PaymentAccountObserverModels/PaymentAccountHelper.php
+++ app/Models/PaymentAccountObserverModels/PaymentAccountHelper.php
@@ -8,21 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentAccountHelper
 {
     private $payment;
+    private $customerPaymentAccount;
     public function __construct($payment){
         $this->payment = $payment;
+        $this->customerPaymentAccount = $payment->customerPaymentAccount;
     }
 
     public function updateRemainingBalance()
     {
-        $customerPaymentAccount = $this->payment->customerPaymentAccount;
-        $payments = Models\Payment::where('customer_payment_account_id', $customerPaymentAccount->id)->get();
+        $payments = Models\Payment::where('customer_payment_account_id', $this->customerPaymentAccount->id)->get();
         $totalAmountPaid = $payments->sum('amount_to_be_paid');
-        $customerPaymentAccount->remaining_balance -= $totalAmountPaid;
+        $this->customerPaymentAccount->remaining_balance -= $totalAmountPaid;
     }
     public function updatePaymentStatus()
     {
-        $customerPaymentAccount = $this->payment->customerPaymentAccount;
-        
+        $this->customerPaymentAccount->payment_status = 'monthly';
     }
     public function updateTermLeft()
     {
@@ -30,6 +30,6 @@ class PaymentAccountHelper
     }
     public function getCustomerPaymentAccount(): Model
     {
-        return $this->payment->customerPaymentAccount;
+        return $this->customerPaymentAccount;
     }
 }
