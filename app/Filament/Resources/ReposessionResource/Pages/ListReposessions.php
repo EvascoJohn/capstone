a53d78654734996b262be9c61b563d902911da33
jeff alteza <jepoyalteza@gmail.com>
2023-12-30 11:25:34 +0800

    create export and date filter

--- app/Filament/Resources/ReposessionResource/Pages/ListReposessions.php
+++ app/Filament/Resources/ReposessionResource/Pages/ListReposessions.php
@@ -4,16 +4,23 @@ namespace App\Filament\Resources\ReposessionResource\Pages;
 
 use App\Enums\ApplicationStatus;
 use App\Filament\Resources\ReposessionResource;
+use App\Traits\ExportToExcelTrait;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListReposessions extends ListRecords
 {
+    use ExportToExcelTrait;
+
     protected static string $resource = ReposessionResource::class;
 
-    public function getTabs(): array
+    public function getHeaderActions(): array
     {
         return [
+            $this->export('Repossessions'),
+
             // Taking this query.
             // "Accounts" => ListRecords\Tab::make()->query(fn ($query) => $query->where('payment_status', 'monthly')),
         ];
