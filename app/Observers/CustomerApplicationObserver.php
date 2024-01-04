fa75bed2ffb8f1296d105ce6182459b5abd78a0b
sub-acc-ni-carlo <johncarloevasco55@gmail.com>
2024-01-04 19:51:12 +0800

    wip: mail and receipts

--- app/Observers/CustomerApplicationObserver.php
+++ app/Observers/CustomerApplicationObserver.php
@@ -26,7 +26,17 @@ class CustomerApplicationObserver
         ]);
 
         Mail::to($customerApplication->applicant_email)
-        ->send(new CustomerApplicationMail("Application Has been created"));
+        ->send(new CustomerApplicationMail(
+            body:   "    I hope this message finds you well. We're thrilled to share some fantastic news with you – your motorcycle application has been successfully reviewed and approved!
+
+    This approval brings you one step closer to hitting the open road on the motorcycle of your dreams. We appreciate the trust you've placed in us and are committed to providing you with an exceptional experience.
+
+    Congratulations once again on the approval of your motorcycle loan! We look forward to helping you embark on thrilling adventures in your new ride.
+
+    Thanks,<br>", 
+            customerName: $customerApplication->applicant_full_name,
+            title:  "# Exciting News: Your Motorcycle Loan Application Has Been Approved!",
+        ));
     }
 
     /**
@@ -57,7 +67,13 @@ class CustomerApplicationObserver
             }
         }
 
-        Mail::to($customerApplication->applicant_email)->send(new CustomerApplicationMail("Application has been been reviewed"));
+        // checks if approved, rejected or resub.
+        Mail::to($customerApplication->applicant_email)
+        ->send(new CustomerApplicationMail(
+            body: "I hope this message finds you well. We're thrilled to share some fantastic news with you – your motorcycle application has been successfully reviewed and approved!\n\nThis approval brings you one step closer to hitting the open road on the motorcycle of your dreams. We appreciate the trust you've placed in us and are committed to providing you with an exceptional experience.\n\nCongratulations once again on the approval of your motorcycle loan! We look forward to helping you embark on thrilling adventures in your new ride.\n\nThanks,", 
+            customerName: $customerApplication->applicant_full_name,
+            title:  "# Exciting News: Your Motorcycle Loan Application Has Been Approved!",
+        ));
     }
 
     /**
