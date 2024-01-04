bf6a4d6c04c9e266b9eba1f3597d33789c2c3b41
sub-acc-ni-carlo <johncarloevasco55@gmail.com>
2024-01-04 10:58:14 +0800

    payment account due data

--- app/Models/PaymentAccountObserverModels/PaymentAccountHelper.php
+++ app/Models/PaymentAccountObserverModels/PaymentAccountHelper.php
@@ -2,16 +2,19 @@
 
 namespace App\Models\PaymentAccountObserverModels;
 
+use App\Enums\ApplicationStatus;
 use App\Models;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class PaymentAccountHelper
 {
-    private $payment;
-    private $customerPaymentAccount;
+    private $payment, $customerApplication,  $customerPaymentAccount;
+
     public function __construct($payment){
         $this->payment = $payment;
         $this->customerPaymentAccount = $payment->customerPaymentAccount;
+        $this->customerApplication = $this->customerPaymentAccount->customerApplication;
     }
 
     public function updateRemainingBalance()
@@ -21,14 +24,56 @@ class PaymentAccountHelper
         $calc = ($this->customerPaymentAccount->original_amount - $totalAmountPaid);
         $this->customerPaymentAccount->remaining_balance = $calc;
     }
+
+    public function updatePaymentAccountDueDate()
+    {
+        // Sets initial due date for the payment account by taking the current time.
+        if($this->customerPaymentAccount->payment_status == 'down payment'){
+            $current_date = Carbon::now();
+            $newDueDate = Models\Payment::calculateDueDate($current_date);
+            $this->customerPaymentAccount->due_date = $newDueDate;
+        }
+        // Takes the existing due date and recalculate the next due.
+        else if($this->customerPaymentAccount->payment_status == 'monthly'){
+            $current_date = $this->customerPaymentAccount->due_date;
+            $newDueDate = Models\Payment::calculateDueDate($current_date);
+            $this->customerPaymentAccount->due_date = $newDueDate;
+        }
+    }
+
     public function updatePaymentStatus()
     {
         $this->customerPaymentAccount->payment_status = 'monthly';
     }
+
+    public function updateCustomerApplicationStatus()
+    {
+        if($this->customerPaymentAccount->payment_status == 'down payment')
+        {
+            $this->customerApplication->application_status = ApplicationStatus::ACTIVE_STATUS;
+            $this->customerPaymentAccount->status = $this->customerApplication->application_status;
+        }
+        else if($this->customerPaymentAccount->payment_status == 'cash')
+        {
+            $this->customerApplication->application_status = ApplicationStatus::CLOSED_STATUS;
+            $this->customerPaymentAccount->status = $this->customerApplication->application_status;
+        }
+    }
+
     public function updateTermLeft()
     {
         $this->customerPaymentAccount->term_left -= $this->payment->term_covered;
+        if($this->customerPaymentAccount->term_left == 0){
+            $this->customerApplication->application_status = ApplicationStatus::CLOSED_STATUS;
+            $this->customerPaymentAccount->status = $this->customerApplication->application_status;
+        }
     }
+
+    public function getCustomerApplication(): Model
+    {
+        return $this->customerApplication;
+    }
+
     public function getCustomerPaymentAccount(): Model
     {
         return $this->customerPaymentAccount;
